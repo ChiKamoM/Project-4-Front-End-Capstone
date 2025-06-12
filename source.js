118d724a952772fbe93a807676961908594cc2b2
Chinasa <kamogelomba@gmail.com>
2025-06-12 15:19:26 +0200

    added javascript

--- source.js
+++ source.js
@@ -48,14 +48,61 @@ nextBttn.addEventListener("click", ()=>{
 
 // Menu modal
 var pizzas = document.querySelectorAll(".menu-item")
-var pizzaIngredients = document.querySelectorAll(".pizza-modal")
+if (pizzas.length>0) {
+   var pizzaIngredients = document.querySelectorAll(".pizza-modal")
 
-pizzas.forEach((pizza, index) => {
+   pizzas.forEach((pizza, index) => {
       pizza.addEventListener("click", () => {
             let current = index;
             pizzaIngredients[index].classList.toggle("hidden")
       })
 });
 
+}
 
-//Form
+
+   //Form
+const form = document.getElementById("form")
+if (form) {
+   let modal = document.querySelector(".modal-container")
+   let nameField = document.querySelector(".name")
+   let surnameField = document.querySelector(".surname")
+   let telNumberField = document.querySelector(".phone-number")
+   let pizzaField = document.querySelector(".selected")  
+   let submit = document.querySelector("#submit")
+   let validate = document.querySelector("#validate")
+   
+   validate.addEventListener("click", ()=>{
+      let pizzaChoice = document.querySelector("#pizzaChoice")
+      let telNumber = document.querySelector("#first-name")
+      let surname = document.querySelector("#surname")
+      let fName = document.querySelector("#first-name")
+      
+      fName = fName.value
+      surname = surname.value
+      telNumber = telNumber.value
+      pizzaChoice = pizzaChoice.value
+      
+      console.log(fName, surname, telNumber, pizzaChoice)
+      if (form.checkValidity() && pizzaChoice != "Choose here") {
+         function inputChoices(field, input) {
+            field.innerText = input
+         }
+         inputChoices(nameField, fName)
+         inputChoices(surnameField, surname)
+         inputChoices(telNumberField, telNumber)
+         inputChoices(pizzaField, pizzaChoice)
+         modal.classList.remove("hidden")
+      } else {
+         console.log("form invalid")
+      }
+
+   })
+   
+   
+}
+   
+
+
+
+
