900b92de1f1f8a30f37499bb7d0336503c5794ca
Chinasa <kamogelomba@gmail.com>
2025-06-17 15:56:55 +0200

    last changes

--- source.js
+++ source.js
@@ -1,5 +1,5 @@
 //Slideshow gallery
- const slides = document.querySelectorAll(".images");
+  const slides = document.querySelectorAll(".images");
 if (slides.length > 0) {
 
 let slideIndex = 0;
@@ -45,7 +45,6 @@ nextBttn.addEventListener("click", ()=>{
 }
 
 
-
 // Menu modal
 var pizzas = document.querySelectorAll(".menu-item")
 if (pizzas.length>0) {
@@ -62,47 +61,75 @@ if (pizzas.length>0) {
 
 
    //Form
-const form = document.getElementById("form")
-if (form) {
-   let modal = document.querySelector(".modal-container")
-   let nameField = document.querySelector(".name")
-   let surnameField = document.querySelector(".surname")
-   let telNumberField = document.querySelector(".phone-number")
-   let pizzaField = document.querySelector(".selected")  
+   const form = document.getElementById("form")
+   if (form) {
+      let modal = document.querySelector(".modal-container")
+   let confirmOrder = document.querySelector(".order-confirmation")
+   let orderPlaced = document.querySelector(".order-placed")
    let submit = document.querySelector("#submit")
    let validate = document.querySelector("#validate")
+   let cancel = document.querySelector("#cancel")
    
-   validate.addEventListener("click", ()=>{
+   
+
+   validate.addEventListener("click", ()=>{ 
+      
+
       let pizzaChoice = document.querySelector("#pizzaChoice")
-      let telNumber = document.querySelector("#first-name")
+
+      if (form.checkValidity() && pizzaChoice != "Choose here") {
+
+      let nameField = document.querySelector(".name")
+      let surnameField = document.querySelector(".surname")
+      let telNumberField = document.querySelector(".phone-number")
+      let pizzaField = document.querySelector(".selected") 
+
+      
+      let telNumber = document.querySelector("#tel-number")
       let surname = document.querySelector("#surname")
-      let fName = document.querySelector("#first-name")
+      let fName = document.querySelector("#first-name")     
       
       fName = fName.value
       surname = surname.value
       telNumber = telNumber.value
       pizzaChoice = pizzaChoice.value
-      
-      console.log(fName, surname, telNumber, pizzaChoice)
-      if (form.checkValidity() && pizzaChoice != "Choose here") {
          function inputChoices(field, input) {
-            field.innerText = input
+            field.innerText = ""
+            field.innerText = input + " "
          }
-         inputChoices(nameField, fName)
+
+         
+         inputChoices(nameField, fName )
          inputChoices(surnameField, surname)
          inputChoices(telNumberField, telNumber)
          inputChoices(pizzaField, pizzaChoice)
          modal.classList.remove("hidden")
-      } else {
-         console.log("form invalid")
+         confirmOrder.classList.remove("hidden")
       }
+         
+         
+      })
+   submit.addEventListener("click", ()=>{
+       const name = document.querySelector(".name")
+       const surname = document.querySelector(".surname")
+       const telNumber = document.querySelector(".phone-number")
+       const pizza = document.querySelector(".selected")
 
+         console.log( "Full name = " + name.innerText + " " + surname.innerText + " Telephone number = " + telNumber.innerText + " Pizza = " + pizza.innerText)
+         form.reset();
+         confirmOrder.classList.add("hidden")
+         orderPlaced.classList.remove("hidden")
+         setTimeout(()=>{
+         modal.classList.add("hidden")
+         orderPlaced.classList.add("hidden")
+      },4000)
    })
+
+
+      
+cancel.addEventListener("click", ()=>{
+ modal.classList.add("hidden")
+      })
+   }
    
    
-}
-   
-
-
-
-
